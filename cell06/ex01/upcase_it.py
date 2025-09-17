f29525d52f2fb97021e74592a3f382704f0098bc
narint181049-wq <narint181049@gmail.com>
2025-09-17 09:35:05 +0000

    cell06-ex02

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,7 @@
+def upcase_it(text):
+    return text.upper()
+
+# Test the method
+if __ame__ ++ "__main__":
+    result = upcase_it("heiio")
+    print(result)
---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,8 @@
+import sys
+def downcase_it(s):
+    return s.lower()
+if len(sys.argv) == 1:
+    print("none")
+    sys.exit()
+for p in sys.argv[1:]:
+    print(dowcase_it(p))
